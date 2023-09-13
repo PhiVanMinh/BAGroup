fef961ef2267b94fba73d246aac12b988e43280c
PhiVanMinh <138870808+PhiVanMinh@users.noreply.github.com>
2023-09-13 17:49:37 +0700

    Add generic repo for dapper

---
+++ Application/Common/Interfaces/IDapperGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface IDapperGenericRepository<T> where T : class
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        bool Add(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+    }
+}
